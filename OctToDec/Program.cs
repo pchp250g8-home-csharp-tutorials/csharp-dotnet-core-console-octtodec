00b7aeabf54ac17dd8e82a5c1b61f9efc7f74e35
Alexander <pchp250g8@tutamail.com>
2026-04-07 15:30:53 +0300

    Add project files.

---
+++ OctToDec/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+const uint MAX_INT = uint.MaxValue;
+var nDecNum = 0;
+var nOctalPower = 1;
+var nMaxOctalLen = Math.Truncate(Math.Log(MAX_INT, 8));
+var oRegEx = new Regex("^[0-7]+$");
+Console.WriteLine("Input a octal number");
+var strLine = Console.ReadLine();
+var nStrLen = strLine.Length;
+var bIsMatch = oRegEx.IsMatch(strLine);
+var bRightString = (nStrLen <= nMaxOctalLen) && (bIsMatch);
+if (!bRightString)
+{
+    Console.WriteLine("Wrong octal number format!!!");
+    Console.Read();
+    return;
+}
+for (int i = 0; i < nStrLen; i++)
+{
+    int nOctalDight = strLine[nStrLen - 1 - i] - '0';
+    nDecNum += (nOctalDight * nOctalPower);
+    nOctalPower *= 8;
+}
+Console.WriteLine("The decenary equivalent of the octal number {0} is {1}", strLine, nDecNum);
+Console.Read();
